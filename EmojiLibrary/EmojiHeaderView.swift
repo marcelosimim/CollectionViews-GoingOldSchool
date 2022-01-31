289039d080a001e02b5c224a90f67067d8d81a86
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2022-01-31 11:54:26 -0300

    conclusion

---
+++ EmojiLibrary/EmojiHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiHeaderView.swift
+//  EmojiLibrary
+//
+//  Created by Marcelo Simim Santos on 31/01/22.
+//
+
+import UIKit
+
+class EmojiHeaderView: UICollectionReusableView {
+    static let reuseIdentifier = String(describing: EmojiHeaderView.self)
+    
+    @IBOutlet weak var textLabel: UILabel!
+}
